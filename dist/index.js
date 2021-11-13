6d1f6fb8da3791d053d946ffe7bd1e47e14062ce
Luczko <wlalukasz@gmail.com>
2021-11-13 17:01:15 +0100

    [03] inquirer added

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const todoItem_1 = require("./todoItem");
 const todoCollection_1 = require("./todoCollection");
+const inquirer = require("inquirer");
 let todos = [
     new todoItem_1.TodoItem(1, "Kupić kwiaty"),
     new todoItem_1.TodoItem(2, "Odebrać buty"),
@@ -9,8 +10,97 @@ let todos = [
     new todoItem_1.TodoItem(4, "Zadzwonić do Janka", true),
 ];
 let collection = new todoCollection_1.TodoCollection("Lukasz", todos);
-console.clear();
-console.log(`Lista ${collection.userName}a` +
-    `(liczba zadań pozostałych do zrobienia: ${collection.getItemsCounts().incomplete})`);
-collection.removeComplete();
-collection.getTodoItems(true).forEach((item) => item.printDetails());
+let showCompleted = true;
+function displayTodoList() {
+    console.log(`Lista ${collection.userName}a ` +
+        `(liczba zadań pozostałych do zrobienia: ${collection.getItemsCounts().incomplete})`);
+    collection.getTodoItems(showCompleted).forEach((item) => item.printDetails());
+}
+var Commands;
+(function (Commands) {
+    Commands["Add"] = "Dodaj nowe zadanie";
+    Commands["Complete"] = "Wykonanie zadania";
+    Commands["Toggle"] = "Poka\u017C lub ukryj wykonane";
+    Commands["Purge"] = "Usu\u0144 wyklonane zadania";
+    Commands["Quit"] = "Koniec";
+})(Commands || (Commands = {}));
+function promptAdd() {
+    console.clear();
+    inquirer
+        .prompt({
+        type: "input",
+        name: "add",
+        message: "Podaj zadanie:",
+    })
+        .then((answers) => {
+        if (answers["add"] !== "") {
+            collection.addTodo(answers["add"]);
+            promptUser();
+        }
+    });
+}
+function promptComplete() {
+    console.clear();
+    inquirer
+        .prompt({
+        type: "checkbox",
+        name: "complete",
+        message: "Oznaczenie zadań jako wykonanych",
+        choices: collection.getTodoItems(showCompleted).map((item) => ({
+            name: item.task,
+            value: item.id,
+            checked: item.complete,
+        })),
+    })
+        .then((answers) => {
+        let completedTasks = answers["complete"];
+        collection
+            .getTodoItems(true)
+            .forEach((item) => collection.markComplete(item.id, completedTasks.find((id) => id === item.id) != undefined));
+        promptUser;
+    });
+}
+function promptUser() {
+    console.clear();
+    displayTodoList();
+    inquirer
+        .prompt({
+        type: "list",
+        name: "command",
+        message: "Wybierz opcję",
+        choices: Object.values(Commands),
+    })
+        .then((answers) => {
+        switch (answers["command"]) {
+            case Commands.Toggle:
+                showCompleted = !showCompleted;
+                promptUser();
+                break;
+            case Commands.Add:
+                promptAdd();
+                break;
+            case Commands.Complete:
+                if (collection.getItemsCounts().incomplete > 0) {
+                    promptComplete();
+                }
+                else {
+                    promptUser();
+                }
+                break;
+            case Commands.Purge:
+                collection.removeComplete();
+                promptUser();
+                break;
+        }
+    });
+}
+promptUser();
+// console.clear();
+// console.log(
+//   `Lista ${collection.userName}a` +
+//     ` (liczba zadań pozostałych do zrobienia: ${
+//       collection.getItemsCounts().incomplete
+//     })`
+// );
+// collection.removeComplete();
+// collection.getTodoItems(true).forEach((item) => item.printDetails());
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { TodoItem } from "./todoItem";
 import { TodoCollection } from "./todoCollection";
+import * as inquirer from "inquirer";
 
 let todos = [
   new TodoItem(1, "Kupić kwiaty"),
@@ -9,14 +10,110 @@ let todos = [
 ];
 
 let collection = new TodoCollection("Lukasz", todos);
+let showCompleted = true;
 
-console.clear();
-console.log(
-  `Lista ${collection.userName}a` +
-    ` (liczba zadań pozostałych do zrobienia: ${
-      collection.getItemsCounts().incomplete
-    })`
-);
+function displayTodoList(): void {
+  console.log(
+    `Lista ${collection.userName}a ` +
+      `(liczba zadań pozostałych do zrobienia: ${
+        collection.getItemsCounts().incomplete
+      })`
+  );
+  collection.getTodoItems(showCompleted).forEach((item) => item.printDetails());
+}
 
-collection.removeComplete();
-collection.getTodoItems(true).forEach((item) => item.printDetails());
+enum Commands {
+  Add = "Dodaj nowe zadanie",
+  Complete = "Wykonanie zadania",
+  Toggle = "Pokaż lub ukryj wykonane",
+  Purge = "Usuń wyklonane zadania",
+  Quit = "Koniec",
+}
+
+function promptAdd(): void {
+  console.clear();
+  inquirer
+    .prompt({
+      type: "input",
+      name: "add",
+      message: "Podaj zadanie:",
+    })
+    .then((answers) => {
+      if (answers["add"] !== "") {
+        collection.addTodo(answers["add"]);
+        promptUser();
+      }
+    });
+}
+
+function promptComplete(): void {
+  console.clear();
+  inquirer
+    .prompt({
+      type: "checkbox",
+      name: "complete",
+      message: "Oznaczenie zadań jako wykonanych",
+      choices: collection.getTodoItems(showCompleted).map((item) => ({
+        name: item.task,
+        value: item.id,
+        checked: item.complete,
+      })),
+    })
+    .then((answers) => {
+      let completedTasks = answers["complete"] as number[];
+      collection
+        .getTodoItems(true)
+        .forEach((item) =>
+          collection.markComplete(
+            item.id,
+            completedTasks.find((id) => id === item.id) != undefined
+          )
+        );
+      promptUser;
+    });
+}
+
+function promptUser(): void {
+  console.clear();
+  displayTodoList();
+  inquirer
+    .prompt({
+      type: "list",
+      name: "command",
+      message: "Wybierz opcję",
+      choices: Object.values(Commands),
+    })
+    .then((answers) => {
+      switch (answers["command"]) {
+        case Commands.Toggle:
+          showCompleted = !showCompleted;
+          promptUser();
+          break;
+        case Commands.Add:
+          promptAdd();
+          break;
+        case Commands.Complete:
+          if (collection.getItemsCounts().incomplete > 0) {
+            promptComplete();
+          } else {
+            promptUser();
+          }
+          break;
+        case Commands.Purge:
+          collection.removeComplete();
+          promptUser();
+          break;
+      }
+    });
+}
+promptUser();
+// console.clear();
+// console.log(
+//   `Lista ${collection.userName}a` +
+//     ` (liczba zadań pozostałych do zrobienia: ${
+//       collection.getItemsCounts().incomplete
+//     })`
+// );
+
+// collection.removeComplete();
+// collection.getTodoItems(true).forEach((item) => item.printDetails());
